cc69fdf5204dde54549a9118ce1ec5fc04fb2355
yanjen <yanjen224@gmail.com>
2023-12-05 11:08:31 +0100

    Fix module path inconsistent problem

--- cmake/fortutf.cmake
+++ cmake/fortutf.cmake
@@ -114,7 +114,12 @@ function(FortUTF_Find_Tests)
         set(FORTUTF FortUTF)
     endif()
     if(NOT TARGET ${FORTUTF})
-        ADD_LIBRARY(${FORTUTF} ${FORTUTF_SRCS})
+        add_library(${FORTUTF} ${FORTUTF_SRCS})
+        set_target_properties(
+            ${FORTUTF}
+            PROPERTIES
+            Fortran_MODULE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/modules
+        )
     endif()
 
     add_executable(${PROJECT_NAME}_Tests ${FORTUTF_PROJECT_SRC_FILES} ${TEST_LIST} ${FORTUTF_PROJECT_TEST_SCRIPT})
